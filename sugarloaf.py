915c98751d2b1ea40408417efa512596c3a9ca4f
mandlebrot <l.lautenschlager@gmail.com>
2018-09-10 13:26:44 -0600

    fix mapping

--- sugarloaf.py
+++ sugarloaf.py
@@ -1,16 +1,19 @@
 #sample data into mesh - Sugarloaf
 import netCDF4 as nc
 import numpy as np
-from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 from scipy import interpolate
 
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
 d = nc.Dataset('dataset')
 fxlon = d.variables['FXLONG'][0,:,:]
 fxlat = d.variables['FXLAT'][0,:,:]
-data = d.variables['TIGN_G'][0,:,:]
+data = d.variables['TIGN_G'][10,:,:]
 d.close()
 
-surf = plt.plot_surface(fxlon,fxlat,data)
+surf = ax.plot_surface(fxlon,fxlat,data,cmap=cm.coolwarm)
 plt.show()
