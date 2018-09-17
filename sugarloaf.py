d0d387990f1191e44cd1078ae6e90feee7d4879a
mandlebrot <l.lautenschlager@gmail.com>
2018-09-17 14:01:00 -0600

    added print lat/lon

--- sugarloaf.py
+++ sugarloaf.py
@@ -15,7 +15,11 @@ d = nc.Dataset('wrfout_d03_2018-09-03_15:00:00')
 fxlon = d.variables['FXLONG'][0,:,:] # boundary masking conditions previously calculated(0:409)
 fxlat = d.variables['FXLAT'][0,:,:]
 data = d.variables['TIGN_G'][10,:,:]
-mask = 
+
+bbox = [(np.amin(fxlon),np.amin(fxlat)),(np.amin(fxlon),np.amax(fxlat)),
+	(np.amax(fxlon),np.amin(fxlat)),(np.amax(fxlon),np.amax(fxlat))]
+print bbox
+
 d.close()
 
 surf = ax.plot_surface(fxlon,fxlat,data,cmap=cm.coolwarm)
