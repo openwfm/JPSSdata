4278c767c83c3992c6ff60d1cbb9bdf9ffe720b6
Angel Farguell Caus <angel.farguell@gmail.com>
2018-10-01 18:06:43 -0600

    Commenting contline.py

--- contline.py
+++ contline.py
@@ -16,37 +16,49 @@ def get_contour_verts(cn):
         paths = []
         # for each separate section of the contour line
         for pp in cc.get_paths():
-            paths.append(pp.vertices)
+            # read all the vertices
+            vv=pp.vertices
+            # add the first element to the end to close the cycle
+            paths.append(np.append(vv,vv[0]))
         contours.append(paths)
 
     return contours
 
 if __name__ == "__main__":
+    # Load all the data in result.mat and mgout.mat
     result=loadmat('result.mat')
     mgout=loadmat('mgout.mat')
+    # Indexing the coordinates into the same as the multigrid solution
     xind=mgout['sm'][0]-1
     yind=mgout['sn'][0]-1
     x=np.array(result['fxlon'])
     xx=x[np.ix_(xind,yind)]
     y=np.array(result['fxlat'])
     yy=y[np.ix_(xind,yind)]
+    # Transpose of the solution is needed to match the coordinates
     zz=np.transpose(mgout['a'])
           
+    # Computing the contours
     cn=plt.contour(xx,yy,zz,20) 
+    # Scaling the time components as in detections
     tscale=mgout['tscale'][0]
     time_scale_num=mgout['time_scale_num'][0]
     time_num=np.array(cn.levels)*tscale+time_scale_num[0]
     time_iso=[time_num2iso(t) for t in time_num]
+    # Generating the coordinates for the contours
     contours=get_contour_verts(cn)
 
     print 'contours are collections of line, each line consisting of poins with x and y coordinates'
+    '''
     for c in contours:
         for cc in c:
             xx=[x[0] for x in cc]
             yy=[y[1] for y in cc]
             plt.scatter(xx,yy)
     plt.show()
+    '''
 
+    # Creating an array of dictionaries for each perimeter
     conts=[Dict({'text':time_iso[k],
         'LineStyle':{ 
             'color':'ff081388',
@@ -59,11 +71,13 @@ if __name__ == "__main__":
         'time_begin':time_iso[k],
         'polygons': contours[k] }) for k in range(0,len(contours))]
 
-    data=Dict({'name':'contours.kmz', 
+    # Creating a dictionary to store the KML file information
+    data=Dict({'name':'contours.kml', 
         'folder_name':'Perimeters'})
     data.update({'contours': conts})
 
+    # Creating the KML file
     contour2kml(data,'perimeters.kml')
 
-    plt.show()
+    print 'perimeters.kml generated'
 
