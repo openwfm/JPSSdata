e9eef6a55db44cfa03c25f359b079ec33e6433d4
Angel Farguell Caus <angel.farguell@gmail.com>
2018-10-12 17:13:06 -0600

    Add to the sugarloaf.py function the GOES16 data download.

--- sugarloaf.py
+++ sugarloaf.py
@@ -1,12 +1,14 @@
 # sample data into mesh - Sugarloaf
 # navigate to /share_home/jmandel/sugarloaf to access sample data
-from JPSSD import retrieve_af_data, read_fire_mesh, time_iso2num, data2json, write_csv, json2kml
+from JPSSD import retrieve_af_data, read_fire_mesh, time_iso2num, data2json, write_csv, json2kml, download_GOES16
 import saveload as sl
 
 fxlon,fxlat,bbox,time_esmf=read_fire_mesh('wrfout_d03_2018-09-03_15:00:00')
 
 # cannot get starting time from wrfout
 time_iso = ("2018-08-15T00:00:00Z", "2018-09-02T00:00:00Z") # tuple, not array
+
+goes=download_GOES16(time_iso)
 
 data=retrieve_af_data(bbox,time_iso)
 
