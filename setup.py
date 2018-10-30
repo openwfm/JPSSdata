409305a183e7aa86181376a2fc0427c5e7014f54
Angel Farguell Caus <angel.farguell@gmail.com>
2018-10-30 12:34:19 -0600

    Cleaning and changing setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import itertools
 maxsize=400 # Max size of the fire mesh
 ut=1 # Upper bound technique, ut=1: Center of the pixel -- ut=2: Ellipse inscribed in the pixel
 lt=1 # Lower bound technique, lt=1: Center of the pixel -- lt=2: Ellipse inscribed in the pixel (very slow)
-mt=3 # Mask technique, mt=1: Ball -- mt=2: Pixel -- mt=3: Ellipse
+mt=2 # Mask technique, mt=1: Ball -- mt=2: Pixel -- mt=3: Ellipse
 if mt<2:
 	dist=8 # If mt=1 (ball neighbours), radius of the balls is R=sqrt(2*dist^2)
 elif mt>2:
@@ -84,54 +84,58 @@ for gran in range(GG):
 	(ff,gg)=nearest_scipy(slon,slat,stree,bounds) ## indices to flattened granule array
 	vfire=np.reshape(fire,np.prod(fire.shape)) ## flaten the fire detection array
 	gfire=vfire[gg]   # the part withing the fire mesh bounds
-	fi=gfire >= 8  # where fire detected - nominal or high confidence 
+	fi=gfire >= 7  # where fire detected - low, nominal or high confidence (all the fire data in the granule)
+	ffi=ff[fi] # indices in the fire mesh where the fire detections are
+	# cheking the confidence level
 	nofi=np.logical_or(gfire == 3, gfire == 5) # where no fire detected
 	unkn=np.logical_not(np.logical_or(fi,nofi)) # where unknown
 	print 'fire detected    %s' % fi.sum()
 	print 'no fire detected %s' % nofi.sum()
 	print 'unknown          %s' % unkn.sum()
 	if fi.any():   # at fire points
-		# indices with fire mask larger than 7
-		afi=gfire >= 7
-		# creating the fire labels of the elements larger than 7
-		gafire=gfire[afi]
-		# indices from the previous elements which are larger than 8
-		flc=gafire >= 8 # fire large confidence indexes
-		# Set upper bounds
-		if ut==1:
-			iu=ff[fi]
-			if pen:
-				conf=sdata[gran][1]['conf_fire'][flc]
-				LP[iu]=conf
-		elif ut==2:
-			# taking lon, lat, scan and track of the fire detections which are >=8
+		conf=sdata[gran][1]['conf_fire'] # confidence of the fire detections
+		flc=conf>50. # fire large confidence indexes
+		if ut>1 or mt>1:
+			# taking lon, lat, scan and track of the fire detections which fire large confidence indexes
 			lon=sdata[gran][1]['lon_fire'][flc]
 			lat=sdata[gran][1]['lat_fire'][flc]
 			scan=sdata[gran][1]['scan_fire'][flc]
 			track=sdata[gran][1]['track_fire'][flc]
+
+		# Set upper bounds
+		if ut==1:
+			# indices with high confidence
+			iu=ffi[flc]
+			# defining penalty in the places with high confidence
+			if pen:
+				UP[iu]=conf[flc]
+		elif ut==2:
 			# creating the indices for all the pixel neighbours of the upper bound
 			iu=neighbor_indices_ellipse(vfxlon,vfxlat,lon,lat,scan,track)
 		else:
 			print 'ERROR: invalid ut option.'
 			sys.exit()
-		U[iu]=ti   # set U to granule time where fire detected
+		U[iu]=ti # set U to granule time where fire detected
+
 		# Set mask
 		if mt==1:
-			kk=neighbor_indices_ball(itree,ff[fi],fxlon.shape,dist) 
+			# creating the indices for all the pixel neighbours of the upper bound indices
+			kk=neighbor_indices_ball(itree,ffi[flc],fxlon.shape,dist) 
 			im=sorted(np.unique([x[0]+x[1]*fxlon.shape[0] for x in vfind[kk]]))
 		elif mt==2:
-			# creating the indices for all the pixel neighbours
+			# creating the indices for all the pixel neighbours of the upper bound indices
 			im=neighbor_indices_pixel(vfxlon,vfxlat,lon,lat,scan,track)
 		elif mt==3:
-			# creating the indices for all the pixel neighbours
+			# creating the indices for all the pixel neighbours of the upper bound indices
 			im=neighbor_indices_ellipse(vfxlon,vfxlat,lon,lat,scan,track,mm)
 		else:
 			print 'ERROR: invalid mt option.'
 			sys.exit()		
-		T[im]=ti       # update mask
+		T[im]=ti # update mask T
 
 	if nofi.any(): # set L at no-fire points and not masked
 		if lt==1:
+			# indices of clear ground
 			jj=np.logical_and(nofi,ti<T[ff])
 			il=ff[jj]
 		elif lt==2:
@@ -140,13 +144,13 @@ for gran in range(GG):
 			lat=sdata[gran][1]['lat_nofire']
 			scan=sdata[gran][1]['scan_nofire']
 			track=sdata[gran][1]['track_nofire']
-			# creating the indices for all the pixel neighbours
+			# creating the indices for all the pixel neighbours of lower bound indices
 			nofi=neighbor_indices_pixel(vfxlon,vfxlat,lon,lat,scan,track)
 			il=np.logical_and(nofi,ti<T)
 		else:
 			print 'ERROR: invalid lt option.'
 			sys.exit()	
-		L[il]=ti
+		L[il]=ti # set L to granule time where fire detected
 		print 'L set at %s points' % jj.sum()
 	t_final = time.time()
 	print 'elapsed time: %ss.' % str(t_final-t_init)
