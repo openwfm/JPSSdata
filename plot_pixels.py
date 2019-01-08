2245405f9c8453ef2c7454fc46d4ddccef08d9af
Angel Farguell Caus <angel.farguell@gmail.com>
2019-01-08 16:49:34 -0700

    Modifications of the plot_pixels to create a plot from all the granules from data file.

--- plot_pixels.py
+++ plot_pixels.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib.collections import PatchCollection
 from matplotlib.transforms import Affine2D
-from JPSSD import read_viirs_files
 from mpl_toolkits.mplot3d import Axes3D
+import saveload as sl
+from interpolation import sort_dates
 import sys
 
 def create_pixels(lons,lats,widths,heights,alphas,color,label):
@@ -40,105 +41,43 @@ def create_pixels(lons,lats,widths,heights,alphas,color,label):
 	col.set_facecolor( [color]*len(lons) )
 	# No lines
 	col.set_linewidth( 0 )
-	# Label
-	#col.set_label(label)
 
 	return col
 
-def angles(lons,lats,ad):
+def center_pixels_plot(g,bounds):
 	"""
-    Approximation of the angles the pixels should be rotated
-    
-    :param lons: array of longitude coordinates at the center of the pixels
-    :param lats: array of latitude coordinates at the center of the pixels
-    :param ad: boolean variable to determine the direction of the angles
-    :return alphas: array of angles of the pixels
-                 
-    Developed in Python 2.7.15 :: Anaconda 4.5.10, on MACINTOSH. 
-    Angel Farguell (angel.farguell@gmail.com) 2018-12-28
-    """
-
-	if ad:
-		dx=lons[0:-1]-lons[1:]
-		if dx[0]<0:
-			dx[0]=dx[dx>0][0]
-		dy=lats[0:-1]-lats[1:]
-		if dy[0]<0:
-			dy[0]=dy[dy>0][0]
-		dlons=np.array([dx[k] if dx[k]>=0 else dx[k-1] for k in range(len(dx))])
-		dlats=np.array([dy[k] if dy[k]>=0 else dy[k-1] for k in range(len(dy))])
-		alphas=np.arctan(dlats/dlons)*180/np.pi
-	else:
-		dx=lons[0:-1]-lons[1:]
-		if dx[0]<0:
-			dx[0]=dx[dx>0][0]
-		dy=lats[0:-1]-lats[1:]
-		if dy[0]>0:
-			dy[0]=dy[dy<0][0]
-		dlons=np.array([dx[k] if dx[k]>=0 else dx[k-1] for k in range(len(dx))])
-		dlats=np.array([dy[k] if dy[k]<=0 else dy[k-1] for k in range(len(dy))])
-		alphas=np.arctan(dlats/dlons)*180/np.pi
-	return alphas
-
-def irregular_pixels_plot(g,bounds):
-	"""
-    Irregular pixels plot: generates a plot of the pixels using along-scan and track dimensions.
+    Center pixels plot: generates a plot of the center of the pixels.
     
     :param g: granule dictionary from read_*_files functions in JPSSD.py
     :param bounds: array with the coordinates of the bounding box of the case
-    :return: 2D plot of the pixels
+    :return: 2D plot of the center of the pixels
                  
     Developed in Python 2.7.15 :: Anaconda 4.5.10, on MACINTOSH. 
     Angel Farguell (angel.farguell@gmail.com) 2018-12-28
     """
 
-	# Angle direction
-	(ii,jj)=np.array(g.lat.shape)/2
-	if g.lat[ii,jj]-g.lat[ii,jj+1]>0:
-		ad=True # Angles should be positive
-	else:
-		ad=False # Angles should be negative
+	fig=plt.figure()
+	ax=fig.add_subplot(111)
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-
-	R=6378  # Earth radius
+    # Size of the center of the pixels
+	size=100
 
 	# Ground pixels
-	lons=g.lon_nofire
-	lats=g.lat_nofire
-	tracks=g.track_nofire
-	scans=g.scan_nofire
-	km_lats=[180/(np.pi*R)]*len(lons)  # 1km in degrees latitude
-	km_lons=km_lats/np.cos(lats*np.pi/180)
-	widths=km_lons*scans
-	heights=km_lats*tracks
-	alphas=angles(lons,lats,ad)
+	lons=np.array(g.lon_nofire)
+	lats=np.array(g.lat_nofire)
 	color=(0,0.59765625,0)
-	col_nofire=create_pixels(lons,lats,widths,heights,alphas,color,'Ground')
-	ax.add_collection(col_nofire)
+	plt.scatter(lons,lats,size,marker='.',color=color)
 
 	# Fire pixels
-	lons=g.lon_fire
-	lats=g.lat_fire
-	tracks=g.track_fire
-	scans=g.scan_fire
-	km_lats=[180/(np.pi*R)]*len(lons)  # 1km in degrees latitude
-	km_lons=km_lats/np.cos(lats*np.pi/180)
-	widths=km_lons*scans
-	heights=km_lats*tracks
-	alphas=[np.mean(alphas)]*len(lons)
+	lons=np.array(g.lon_fire)
+	lats=np.array(g.lat_fire)
 	color=(0.59765625,0,0)
-	col_fire=create_pixels(lons,lats,widths,heights,alphas,color,'Fire')
-	ax.add_collection(col_fire)
+	plt.scatter(lons,lats,size,marker='.',color=color)
 
-	# Set axis
 	ax.set_xlim(bounds[0],bounds[1])
 	ax.set_ylim(bounds[2],bounds[3])
 
-	plt.show()
-
-def regular_pixels_plot(g,bounds):
+def pixels_plot(g,bounds):
 	"""
     Regular pixels plot: generates a plot of the pixels using a regular grid by distances.
     
@@ -251,30 +190,41 @@ def regular_pixels_plot(g,bounds):
 
 	#cb.set_ticklabels(colors)
 
-	plt.show()
+def perror():
+	print "Error: python %s pixel_plot_type" % sys.argv[0] 
+	print "  - Center pixels plot: 0."
+	print "  - Regular pixels plot: 1."
 
 if __name__ == "__main__":
 	if len(sys.argv)!=2:
-		print "Error: python %s pixel_plot_type" % sys.argv[0] 
-		print "  - Irregular pixels plot: 0."
-		print "  - Regular pixels plot: 1."
+		perror()
 		sys.exit(1)
 	else:
 		if sys.argv[1] not in ['0','1']:
-			print "Error: python %s pixel_plot_type" % sys.argv[0] 
-			print "  - Irregular pixels plot: 0."
-			print "  - Regular pixels plot: 1."
-			print sys.argv[1]
+			perror()
+			print "Invalid value given: %s" % sys.argv[1]
 			sys.exit(1)
 
-	# Upload data
-	files=Dict({})
-	files.geo='VNP03MODLL.A2018320.2036.001.2018321040917.h5'
-	files.fire='VNP14.A2018320.2036.001.2018321030603.nc'
-	bounds=[-122.042564, -120.96382, 39.34862, 40.174423]
-	g=read_viirs_files(files,bounds)
+	try:
+		# Upload data
+		print 'Loading data...'
+		data,fxlon,fxlat,time_num=sl.load('data')
+	except Exception:
+		print "Error: No data file in the directory. First run a case using case.py."
+		sys.exit(1)
+
+	granules=sort_dates(data)
+	bounds=[fxlon.min(),fxlon.max(),fxlat.min(),fxlat.max()]
+
+	print 'Plotting data...'
 	# Plot pixels
-	if int(sys.argv[1]):
-		regular_pixels_plot(g,bounds)
+	if sys.argv[1] is '0':
+		for g in granules:
+			center_pixels_plot(g[1],bounds)
+			plt.title('Granule %s' % g[0])
 	else:
-		irregular_pixels_plot(g,bounds)
+		for g in granules:
+			pixels_plot(g[1],bounds)
+			plt.title('Granule %s' % g[0])
+
+	plt.show()
