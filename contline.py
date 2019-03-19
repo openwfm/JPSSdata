3f97497ac4b3730e545aa600d9ea4d5daca0dc85
Angel Farguell Caus <angel.farguell@gmail.com>
2019-03-19 13:30:51 -0600

    Fixing a couple of typos in contline python functions

--- contline.py
+++ contline.py
@@ -1,5 +1,3 @@
-import warnings
-warnings.filterwarnings("ignore")
 # following https://stackoverflow.com/questions/18304722/python-find-contour-lines-from-matplotlib-pyplot-contour
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +8,30 @@ import time
 from JPSSD import time_num2iso
 from contour2kml import contour2kml
 
-def get_contour_verts(cn):
+def get_contour_verts(xx, yy, zz, time_num_granules, contour_dt_hours=6, contour_dt_init=6, contour_dt_final=6, plot_contours=False):
+    # Computing the levels
+    # Datetimes for the first and last granule
+    dt1=datetime.datetime.fromtimestamp(time_num_granules[0])
+    dt2=datetime.datetime.fromtimestamp(time_num_granules[-1])
+    # Starting 6 hours before the first granule and finishing 6 hours after the last granule
+    dti=dt1-datetime.timedelta(hours=contour_dt_init)
+    dtf=dt2+datetime.timedelta(hours=contour_dt_final)
+    # Number of periods of 6 hours we need to compute rounded
+    d=dtf-dti
+    hours=d.total_seconds()/3600
+    M=int(np.round((hours+1)/contour_dt_hours ))
+    # Datetimes where we are going to compute the levels
+    dts=[dti+datetime.timedelta(hours=k*contour_dt_hours) for k in range(1,M)]
+    levels=[time.mktime(t.timetuple()) for t in dts]
+
+    # Computing the contours
+    cn=plt.contour(xx,yy,zz,levels=levels)
+
+    # Scaling the time components as in detections
+    time_num=np.array(cn.levels)
+    time_iso=[time_num2iso(t) for t in time_num]
+
+    # Generating the coordinates for the contours
     contours = []
     # for each contour line
     for cc in cn.collections:
@@ -21,7 +42,35 @@ def get_contour_verts(cn):
             paths.append(pp.vertices)
         contours.append(paths)
 
-    return contours
+    # Plotting or not the contour lines
+    if plot_contours:
+        print 'contours are collections of line, each line consisting of poins with x and y coordinates'
+        for c in contours:
+            for cc in c:
+                xx=[x[0] for x in cc]
+                yy=[y[1] for y in cc]
+                plt.scatter(xx,yy)
+        plt.show()
+
+    # Creating an array of dictionaries for each perimeter
+    conts=[Dict({'text':time_iso[k],
+        'LineStyle':{
+            'color':'ff081388',
+            'width':'2.5',
+        },
+        'PolyStyle':{
+            'color':'66000086',
+            'colorMode':'random'
+        },
+        'time_begin':time_iso[k],
+        'polygons': contours[k] }) for k in range(0,len(contours))]
+
+    # Creating a dictionary to store the KML file information
+    data=Dict({'name':'contours.kml',
+        'folder_name':'Perimeters'})
+    data.update({'contours': conts})
+
+    return data
 
 if __name__ == "__main__":
     print 'Loading the data...'
@@ -41,58 +90,8 @@ if __name__ == "__main__":
 
     print 'Computing the contours...'
     # Granules numeric times
-    time_num_granules=result['time_num_granules'][0]
-    # Datetimes for the first and last granule
-    dt1=datetime.datetime.fromtimestamp(time_num_granules[0])
-    dt2=datetime.datetime.fromtimestamp(time_num_granules[-1])
-    # Starting 6 hours before the first granule and finishing 6 hours after the last granule
-    dti=dt1-datetime.timedelta(hours=6)
-    dtf=dt2+datetime.timedelta(hours=6)
-    # Number of periods of 6 hours we need to compute rounded
-    d=dtf-dti
-    hours=d.total_seconds()/3600
-    contour_dt_hours = 24
-    M=int(np.round((hours+1)/contour_dt_hours ))
-    # Datetimes where we are going to compute the levels
-    dts=[dti+datetime.timedelta(hours=k*contour_dt_hours) for k in range(1,M)]
-    levels=[time.mktime(t.timetuple()) for t in dts]
-    # Computing the contours
-    cn=plt.contour(xx,yy,zz,levels=levels)
-    # Scaling the time components as in detections
-    time_num=np.array(cn.levels)
-    time_iso=[time_num2iso(t) for t in time_num]
-    # Generating the coordinates for the contours
-    contours=get_contour_verts(cn)
-
-    # Plotting or not the contour lines
-    plot=False
-    if plot:
-        print 'contours are collections of line, each line consisting of poins with x and y coordinates'
-        for c in contours:
-            for cc in c:
-                xx=[x[0] for x in cc]
-                yy=[y[1] for y in cc]
-                plt.scatter(xx,yy)
-        plt.show()
-
-    print 'Creating the dictionary...'
-    # Creating an array of dictionaries for each perimeter
-    conts=[Dict({'text':time_iso[k],
-        'LineStyle':{
-            'color':'ff081388',
-            'width':'2.5',
-        },
-        'PolyStyle':{
-            'color':'66000086',
-            'colorMode':'random'
-        },
-        'time_begin':time_iso[k],
-        'polygons': contours[k] }) for k in range(0,len(contours))]
-
-    # Creating a dictionary to store the KML file information
-    data=Dict({'name':'contours.kml',
-        'folder_name':'Perimeters'})
-    data.update({'contours': conts})
+    time_num_granules = result['time_num_granules'][0]
+    data = get_contour_verts(xx, yy, zz, time_num_granules, contour_dt_hours=24)
 
     print 'Creating the KML file...'
     # Creating the KML file
--- contlinesvm.py
+++ contlinesvm.py
@@ -1,98 +1,24 @@
-import warnings
-warnings.filterwarnings("ignore")
-# following https://stackoverflow.com/questions/18304722/python-find-contour-lines-from-matplotlib-pyplot-contour
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.io import loadmat
-from utils import *
-import datetime
-import time
-from JPSSD import time_num2iso
+from contline import get_contour_verts
 from contour2kml import contour2kml
-import sys
 
-def get_contour_verts(cn):
-    contours = []
-    # for each contour line
-    for cc in cn.collections:
-        paths = []
-        # for each separate section of the contour line
-        for pp in cc.get_paths():
-            # read all the vertices
-            paths.append(pp.vertices)
-        contours.append(paths)
+print 'Loading the data...'
+# Load all the data in svm.mat
+svm=loadmat('svm.mat')
+xx=np.array(svm['fxlon'])
+yy=np.array(svm['fxlat'])
+tscale=svm['tscale'][0]
+time_scale_num=svm['time_scale_num'][0]
+zz=svm['fmc_g']*tscale+time_scale_num[0]
 
-    return contours
+print 'Computing the contours...'
+# Granules numeric times
+time_num_granules = svm['time_num_granules'][0]
+data = get_contour_verts(xx, yy, zz, time_num_granules, contour_dt_hours=6, contour_dt_init=6, contour_dt_final=6)
 
-if __name__ == "__main__":
-    print 'Loading the data...'
-    # Load all the data in svm.mat
-    svm=loadmat('svm.mat')
-    xx=np.array(svm['fxlon'])
-    yy=np.array(svm['fxlat'])
-    tscale=svm['tscale'][0]
-    time_scale_num=svm['time_scale_num'][0]
-    zz=svm['fmc_g']*tscale+time_scale_num[0]
+print 'Creating the KML file...'
+# Creating the KML file
+contour2kml(data,'perimeters_svm.kml')
 
-    print 'Computing the contours...'
-    # Granules numeric times
-    time_num_granules=svm['time_num_granules'][0]
-    #sys.exit()
-    # Datetimes for the first and last granule
-    dt1=datetime.datetime.fromtimestamp(time_num_granules[0])
-    dt2=datetime.datetime.fromtimestamp(time_num_granules[-1])
-    # Starting 6 hours before the first granule and finishing 6 hours after the last granule
-    dti=dt1-datetime.timedelta(hours=6)
-    dtf=dt2+datetime.timedelta(hours=6)
-    # Number of periods of 6 hours we need to compute rounded
-    d=dtf-dti
-    hours=d.total_seconds()/3600
-    contour_dt_hours = 6
-    M=int(np.round((hours+1)/contour_dt_hours ))
-    # Datetimes where we are going to compute the levels
-    dts=[dti+datetime.timedelta(hours=k*contour_dt_hours) for k in range(1,M)]
-    levels=[time.mktime(t.timetuple()) for t in dts]
-    # Computing the contours
-    cn=plt.contour(xx,yy,zz,levels=levels)
-    # Scaling the time components as in detections
-    time_num=np.array(cn.levels)
-    time_iso=[time_num2iso(t) for t in time_num]
-    # Generating the coordinates for the contours
-    contours=get_contour_verts(cn)
-
-    # Plotting or not the contour lines
-    plot=True
-    if plot:
-        print 'contours are collections of line, each line consisting of poins with x and y coordinates'
-        for c in contours:
-            for cc in c:
-                xx=[x[0] for x in cc]
-                yy=[y[1] for y in cc]
-                plt.scatter(xx,yy)
-        plt.show()
-
-    print 'Creating the dictionary...'
-    # Creating an array of dictionaries for each perimeter
-    conts=[Dict({'text':time_iso[k],
-        'LineStyle':{
-            'color':'ff081388',
-            'width':'2.5',
-        },
-        'PolyStyle':{
-            'color':'66000086',
-            'colorMode':'random'
-        },
-        'time_begin':time_iso[k],
-        'polygons': contours[k] }) for k in range(0,len(contours))]
-
-    # Creating a dictionary to store the KML file information
-    data=Dict({'name':'contours.kml',
-        'folder_name':'Perimeters'})
-    data.update({'contours': conts})
-
-    print 'Creating the KML file...'
-    # Creating the KML file
-    contour2kml(data,'perimeters.kml')
-
-    print 'perimeters.kml generated'
-
+print 'perimeters_svm.kml generated'
