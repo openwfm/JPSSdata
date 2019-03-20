e819146d8015c885002fb398d13ece5fee9d1bda
Angel Farguell Caus <angel.farguell@gmail.com>
2019-03-20 14:06:04 -0600

    Don't process data processed before

--- make_mat.py
+++ make_mat.py
@@ -28,62 +28,66 @@ for gran in list(data):
 	date=splitted[1][3:8]+splitted[2]+'00'
 	file_name = prefixes[prefix]+'.'+date+'.tif.mat'
 
-	# Creating meshgrid where to interpolate the data
-	bounds = [data[gran].lon.min(),data[gran].lon.max(),data[gran].lat.min(),data[gran].lat.max()]
-	lons_interp = np.arange(bounds[0],bounds[1],res)
-	lats_interp = np.arange(bounds[2],bounds[3],res)
-	lons_interp,lats_interp = np.meshgrid(lons_interp,lats_interp)
+	if not os.path.isfile(file):
+		# Creating meshgrid where to interpolate the data
+		bounds = [data[gran].lon.min(),data[gran].lon.max(),data[gran].lat.min(),data[gran].lat.max()]
+		lons_interp = np.arange(bounds[0],bounds[1],res)
+		lats_interp = np.arange(bounds[2],bounds[3],res)
+		lons_interp,lats_interp = np.meshgrid(lons_interp,lats_interp)
 
-	# Creating geotransform array with necessary geolocation information
-	geotransform = [bounds[0],res,rot,bounds[3],rot,res]
+		# Creating geotransform array with necessary geolocation information
+		geotransform = [bounds[0],res,rot,bounds[3],rot,res]
 
-	# Approximation of the radius for the tree
-	dlon = abs(data[gran].lon[1,1]-data[gran].lon[0,0])
-	dlat = abs(data[gran].lat[1,1]-data[gran].lat[0,0])
-	radius = max(dlon,dlat)
-	print 'radius =',radius
+		# Approximation of the radius for the tree
+		dlon = abs(data[gran].lon[1,1]-data[gran].lon[0,0])
+		dlat = abs(data[gran].lat[1,1]-data[gran].lat[0,0])
+		radius = max(dlon,dlat)
+		print 'radius =',radius
 
-	# Flatten granule data into 1d arrays
-	lons = np.reshape(data[gran].lon,np.prod(data[gran].lon.shape))
-	lats = np.reshape(data[gran].lat,np.prod(data[gran].lat.shape))
-	fires = np.reshape(data[gran].fire,np.prod(data[gran].fire.shape)).astype(np.int8)
+		# Flatten granule data into 1d arrays
+		lons = np.reshape(data[gran].lon,np.prod(data[gran].lon.shape))
+		lats = np.reshape(data[gran].lat,np.prod(data[gran].lat.shape))
+		fires = np.reshape(data[gran].fire,np.prod(data[gran].fire.shape)).astype(np.int8)
 
-	# Making tree
-	print '> Making the tree...'
-	t1 = time()
-	tree = spatial.cKDTree(np.column_stack((lons,lats)))
-	t2 = time()
-	print 'elapsed time: ',abs(t2-t1),'s'
-	glons = np.reshape(lons_interp,np.prod(lons_interp.shape))
-	glats = np.reshape(lats_interp,np.prod(lats_interp.shape))
-	print '> Interpolating the data...'
-	t1 = time()
-	indexes = np.array(tree.query_ball_point(np.column_stack((glons,glats)),radius))
-	t2 = time()
-	print 'elapsed time: ',abs(t2-t1),'s'
-	filtered_indexes = np.array([index[0] if len(index) > 0 else np.nan for index in indexes])
-	fire1d = [fires[int(ii)] if not np.isnan(ii) else 0 for ii in filtered_indexes]
-	fires_interp = np.reshape(fire1d,lons_interp.shape)
+		# Making tree
+		print '> Making the tree...'
+		t1 = time()
+		tree = spatial.cKDTree(np.column_stack((lons,lats)))
+		t2 = time()
+		print 'elapsed time: ',abs(t2-t1),'s'
+		glons = np.reshape(lons_interp,np.prod(lons_interp.shape))
+		glats = np.reshape(lats_interp,np.prod(lats_interp.shape))
+		print '> Interpolating the data...'
+		t1 = time()
+		indexes = np.array(tree.query_ball_point(np.column_stack((glons,glats)),radius))
+		t2 = time()
+		print 'elapsed time: ',abs(t2-t1),'s'
+		filtered_indexes = np.array([index[0] if len(index) > 0 else np.nan for index in indexes])
+		fire1d = [fires[int(ii)] if not np.isnan(ii) else 0 for ii in filtered_indexes]
+		fires_interp = np.reshape(fire1d,lons_interp.shape)
 
-	plot = False
-	if plot:
-		print '> Plotting the data...'
-		#number of elements in arrays to plot
-		nums = 100
-		nums1 = 50
-		fig1, (ax1,ax2) = plt.subplots(nrows = 2, ncols =1)
-		ax1.pcolor(lons_interp[0::nums1],lats_interp[0::nums1],fires_interp[0::nums1])
-		ax2.scatter(lons[0::nums],lats[0::nums],c=fires[0::nums],edgecolors='face')
-		plt.show()
+		plot = False
+		if plot:
+			print '> Plotting the data...'
+			#number of elements in arrays to plot
+			nums = 100
+			nums1 = 50
+			fig1, (ax1,ax2) = plt.subplots(nrows = 2, ncols =1)
+			ax1.pcolor(lons_interp[0::nums1],lats_interp[0::nums1],fires_interp[0::nums1])
+			ax2.scatter(lons[0::nums],lats[0::nums],c=fires[0::nums],edgecolors='face')
+			plt.show()
 
-	print '> Saving the data...'
-	t1 = time()
-	result = {'data': fires_interp.astype(np.int8),'geotransform':geotransform}
-	sio.savemat(file_name,mdict = result)
-	t2 = time()
-	print 'elapsed time: ',abs(t2-t1),'s'
+		print '> Saving the data...'
+		t1 = time()
+		result = {'data': fires_interp.astype(np.int8),'geotransform':geotransform}
+		sio.savemat(file_name,mdict = result)
+		t2 = time()
+		print 'elapsed time: ',abs(t2-t1),'s'
 
-	print '> File saved as ',file_name
+		print '> File saved as ',file_name
+	else:
+		print '> File already exists ',file_name
+
 	tg2 = time()
 	print '>> Elapsed time for the granule: ',abs(tg2-tg1),'s'
 	sys.stdout.flush()
