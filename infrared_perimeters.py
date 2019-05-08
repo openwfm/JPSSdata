6a7bad82e4a522ed23bcef26d30abbf63855a681
Angel Farguell Caus <angel.farguell@gmail.com>
2019-05-08 15:37:17 -0600

    adding acq_date and acq_time to perimeter and ignition information

--- infrared_perimeters.py
+++ infrared_perimeters.py
@@ -19,6 +19,8 @@ def process_ignitions(igns):
             time_datetime = time_iso2datetime(time_iso)
             time_data = '_A%04d%03d_%02d%02d' % (time_datetime.year, time_datetime.timetuple().tm_yday,
                                         time_datetime.hour, time_datetime.minute)
+            acq_date = '%04d%02d%02d' % (time_datetime.year, time_datetime.month, time_datetime.day)
+            acq_time = '%02d%02d' % (time_datetime.hour, time_datetime.minute)
         except Exception as e:
             print 'Error: bad ignition %s specified.' % igns
             print 'Exception: %s.' % e
@@ -26,7 +28,8 @@ def process_ignitions(igns):
         ignitions.update({prefix + time_data: Dict({'lon': lons, 'lat': lats,
                                 'fire': np.array(9*np.ones(lons.shape)), 'conf_fire': np.array(100*np.ones(lons.shape)),
                                 'lon_fire': lons, 'lat_fire': lats, 'scan_fire': scan*np.ones(lons.shape),
-                                'track_fire': track*np.ones(lons.shape), 'time_iso': time_iso, 'time_num': time_num})})
+                                'track_fire': track*np.ones(lons.shape), 'time_iso': time_iso, 'time_num': time_num,
+                                'acq_date': acq_date, 'acq_time': acq_time})})
     return ignitions
 
 def process_infrared_perimeters(dst,plot=False):
@@ -53,6 +56,8 @@ def process_infrared_perimeters(dst,plot=False):
                 time_datetime = time_iso2datetime(time_iso)
                 time_data = '_A%04d%03d_%02d%02d' % (time_datetime.year, time_datetime.timetuple().tm_yday,
                                                     time_datetime.hour, time_datetime.minute)
+                acq_date = '%04d%02d%02d' % (time_datetime.year, time_datetime.month, time_datetime.day)
+                acq_time = '%02d%02d' % (time_datetime.hour, time_datetime.minute)
                 polygons = re.findall(r'<Polygon>(.*?)</Polygon>',f_str,re.DOTALL)
                 buckets = [re.split('\r\n\s+',re.findall(r'<coordinates>(.*?)</coordinates>',p,re.DOTALL)[0])[1:] for p in polygons]
                 coordinates = [np.array(re.split(',',b)[0:2]).astype(float) for bucket in buckets for b in bucket]
@@ -67,7 +72,8 @@ def process_infrared_perimeters(dst,plot=False):
             perimeters.update({prefix + time_data: Dict({'file': file, 'lon': lons, 'lat': lats,
                             'fire': np.array(9*np.ones(lons.shape)), 'conf_fire': np.array(100*np.ones(lons.shape)),
                             'lon_fire': lons, 'lat_fire': lats, 'scan_fire': scan*np.ones(lons.shape),
-                            'track_fire': track*np.ones(lons.shape), 'time_iso': time_iso, 'time_num': time_num})})
+                            'track_fire': track*np.ones(lons.shape), 'time_iso': time_iso, 'time_num': time_num,
+                            'acq_date': acq_date, 'acq_time': acq_time})})
         if plot:
             plt.show()
     else:
