33aba617daff64c0cce33dc136834a1ef03986e9
Angel Farguell Caus <angel.farguell@gmail.com>
2019-08-27 14:48:19 -0600

    solving error when saveload using python 3

--- saveload.py
+++ saveload.py
@@ -6,7 +6,7 @@ def save(obj,file):
     :param obj: object to be saved
     :param file: file name
     """
-    with open(file,'w') as f:
+    with open(file,'wb') as f:
        pickle.dump(obj,f,protocol=-1)
 
 def load(file):
@@ -14,7 +14,7 @@ def load(file):
     :param file: file name
     :return: the object read
     """
-    with open(file,'r') as f:
+    with open(file,'rb') as f:
        return pickle.load(f)
 
 
