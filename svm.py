4e8f3370066ba0d5bbc129cf2d1d3320c7c24f52
Angel Farguell Caus <angel.farguell@gmail.com>
2020-08-21 15:49:02 -0600

    fixing a typo

--- svm.py
+++ svm.py
@@ -84,7 +84,7 @@ def preprocess_data_svm(data, scale, minconf=80.):
                                  gran[1]['lat_nofire'] <= bf[3])))
         xg = np.c_[(gran[1]['lon_nofire'][mask],gran[1]['lat_nofire'][mask],np.repeat(tt,mask.sum()))]
         print 'no fire detections: %g' % len(xg)
-        coarsening = 1 + max((len(xg)-len(xf))//ngran,0)
+        coarsening = int(1 + max((len(xg)-len(xf))//ngran,0))
         print 'no fire coarsening: %d' % coarsening
         print 'no fire detections reduction: %g' % len(xg[::coarsening])
         XX[1].append(xg[::coarsening])
