a62a8421ddfe953ffa6dd0421891054b5d7bf71b
Jan Mandel <jan.mandel@gmail.com>
2020-09-27 19:14:41 -0600

    reading wrfout, inetrpolating

--- svm_driver.py
+++ svm_driver.py
@@ -1,23 +1,29 @@
 from svm import SVM3
 import numpy as np
 from scipy.io import loadmat,savemat
+from scipy import interpolate
 from JPSSD import read_fire_mesh
 import sys
 import datetime as dt
 from contour2kml import contour2kml
+from time import time
+
+t_init = time()
 
 m = loadmat('fire.mat')
 search = True
 Xg = m['Xg'];
 Xf = m['Xg'];
 tscale = m['tscale']
-epoch = m['epoch'].flatten()
+epoch = m['epoch'].ravel()
+bbox = m['bbox'].ravel()
 # time = col 3 of Xg Xf is in tscale seconds since epoch
 epoch_dt = dt.datetime(year=epoch[0],month=epoch[1],day=epoch[2],
                        hour=epoch[3],minute=epoch[4],second=epoch[5])
 svm_file = 'svm.mat'
+svm_file2 = 'svm_out.mat'
 
-for exp in (1,):
+for exp in ():
 
     def exp1():
         C = 10.
@@ -39,14 +45,14 @@ for exp in (1,):
     y = np.concatenate((-np.ones(len(Xg)), np.ones(len(Xf))))
 
     # Running SVM classification
-    F = SVM3(X,y,C=C,kgam=kgam,search=search,plot_result=True,plot_data=True)
-
+    Fx, Fy, Fz = SVM3(X,y,C=C,kgam=kgam,search=search,plot_result=True,plot_data=True)
+ 
     sys.stdout.flush()
-    tign_g = np.array(F[2])
+    tign_g = np.array(Fz)
     # Creating the dictionary with the results
     svm = { 'X': np.array(X), 'y': np.array(y), 'C': np.array(C),
-    		'fxlon': np.array(F[0]), 'fxlat': np.array(F[1]), 
-    		'Z': np.array(F[2]), 'tign_g': np.array(tign_g), 
+    		'fxlon': np.array(Fx), 'fxlat': np.array(Fy), 
+    		'Z': np.array(Fz), 'tign_g': np.array(tign_g), 
     		'C': C, 'kgam': kgam, 
                 'tscale': tscale, 'epoch':epoch,'bbox':bbox}
     # time is epoch + tscale * tign_g
@@ -54,44 +60,58 @@ for exp in (1,):
     savemat(svm_file, mdict=svm)
     print ('The results are saved in %s' % svm_file)
     
+ 
+print ('Loading from %s' % svm_file)
 svm = loadmat(svm_file)
-print ('Loading from %s' % smf_file)
+#print(svm)
+
+print('svm bbox %s %s %s %s %s %s' % (
+    np.amin(svm['fxlon']), np.amax(svm['fxlon']),
+    np.amin(svm['fxlat']), np.amax(svm['fxlat']),
+    np.amin(svm['Z']), np.amax(svm['Z'])))
+
+tscale=svm['tscale']
+epoch=svm['epoch'].ravel()
+epoch_dt = dt.datetime(year=epoch[0],month=epoch[1],day=epoch[2],
+                       hour=epoch[3],minute=epoch[4],second=epoch[5])
 
 # still have bbox and epoch_dt from above
-# fxlon,fxlat,bbox,time_esmf=read_fire_mesh('wrfout')
+try:
+    fxlon,fxlat,bbox,time_esmf=read_fire_mesh('wrfout')
+    time_dt = dt.datetime.strptime(time_esmf,'%Y-%m-%d_%H:%M:%S')
+except:
+    print('cannot read wrfout, using existing values')
+    bbox = svm['bbox']
+    fxlon,fxlat=np.meshgrid(np.linspace(bbox[0],bbox[1],num=100),
+                        np.linspace(bbox[2],bbox[3],num=111))
+    time_dt = epoch_dt
+
 # assuming time_esmf is the beginning of the simulation
 # esmf format is YYYY-MM-DD_hh:mm:ss
 
-# wrfout not available - making it up, testing only
-time_esmf = '2019-07-24_22:00:00'
-fxlon,fxlat=np.meshgrid(np.linspace(bbox[0],bbox[1],num=100),
-                        np.linspace(bbox[2],bbox[3],num=111))
-#end making it up
 
-time_dt = dt.datetime.strptime(time_esmf,'%Y-%m-%d_%H:%M:%S')
 # Interpolation of tign_g
+fire_interp = True
+notnan = True
 if fire_interp:
-	try:
 		print '>> Interpolating the results in the fire mesh'
 		t_interp_1 = time()
-		points = np.c_[np.ravel(F[0]),np.ravel(F[1])]
-		values = np.ravel(tign_g)*tscale + (epoch_dt - time_dt).total_seconds()
+		points = (svm['fxlon'].ravel(),svm['fxlat'].ravel())
+		values = (svm['Z']*tscale + (epoch_dt - time_dt).total_seconds()).ravel()
 		tign_g_interp = interpolate.griddata(points,values,(fxlon,fxlat))
 		t_interp_2 = time()
 		if notnan:
 			with np.errstate(invalid='ignore'):
 				tign_g_interp[np.isnan(tign_g_interp)] = np.nanmax(tign_g_interp)
 		print 'elapsed time: %ss.' % str(abs(t_interp_2-t_interp_1))
-		svm.update({'fxlon_interp': np.array(fxlon), 
+                svm2 = svm
+		svm2.update({'fxlon_interp': np.array(fxlon), 
 			'fxlat_interp': np.array(fxlat),
 			'tign_g_interp': np.array(tign_g_interp)})
-	except:
-		print 'Warning: longitudes and latitudes from the original grid are not defined...'
-		print '%s file is not compatible with fire_interp=True! Run again the experiment from the begining.' % bounds_file
 
 # Save resulting file
-savemat(svm_file, mdict=svm)
-print 'The results are saved in ' + svm_file
+savemat(svm_file2, mdict=svm)
+print 'The results are saved in file %s' % svm_file2
 
 print ''
 print '>> DONE <<'
