41233ac843edeb9e770ae4d3af4e3108cd946f13
Angel Farguell Caus <angel.farguell@gmail.com>
2020-12-04 23:47:29 -0700

    list process files

---
+++ ls_process.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# binary files
+ls data result perimeters decision 2> /dev/null
+# matlab files
+ls result.mat svm.mat 2> /dev/null
+# kml files
+ls fire_detections.kml nofire.kml perimeters_svm.kml 2> /dev/null
+# kmz files
+ls googlearth.kmz 2> /dev/null
+# png files
+ls original_data.png scaled_data.png decision.png support.png tign_g.png result.png 2> /dev/null
